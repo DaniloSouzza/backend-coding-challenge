9f37890af31815acc7339351468635b38d49f091
Danilo Paes <danilo.souza@ntconsult.com.br>
2022-10-10 10:30:59 +0200

    feat: implementing the api endpoint functionality

--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -9,19 +9,35 @@ providing a search across all public Gists for a given Github account.
 """
 
 import requests
-from flask import Flask, jsonify, request
+
+from flask import Flask, Response, jsonify, request
+from .utils.gists_helpers import GistsHelper
+from .utils.validations import PayloadValidator, ResponseValidator
+from .utils.exceptions import (
+    NoPatternInformed,
+    NoUsernameInformed,
+    InvalidPattern,
+    UserNotFound,
+)
 
 
 app = Flask(__name__)
 
+API_EXCEPTIONS = (
+    NoPatternInformed,
+    NoUsernameInformed,
+    UserNotFound,
+    InvalidPattern,
+)
+
 
 @app.route("/ping")
-def ping():
+def ping() -> str:
     """Provide a static response to a simple GET request."""
     return "pong"
 
 
-def gists_for_user(username: str):
+def gists_for_user(username: str) -> dict:
     """Provides the list of gist metadata for a given user.
 
     This abstracts the /users/:username/gist endpoint from the Github API.
@@ -35,13 +51,15 @@ def gists_for_user(username: str):
         The dict parsed from the json response from the Github API.  See
         the above URL for details of the expected structure.
     """
-    gists_url = 'https://api.github.com/users/{username}/gists'.format(username=username)
+    gists_url = "https://api.github.com/users/{username}/gists".format(
+        username=username
+    )
     response = requests.get(gists_url)
     return response.json()
 
 
-@app.route("/api/v1/search", methods=['POST'])
-def search():
+@app.route("/api/v1/search", methods=["POST"])
+def search() -> Response:
     """Provides matches for a single pattern across a single users gists.
 
     Pulls down a list of all gists for a given user and then searches
@@ -52,25 +70,34 @@ def search():
         object contains the list of matches along with a 'status' key
         indicating any failure conditions.
     """
-    post_data = request.get_json()
-
-    username = post_data['username']
-    pattern = post_data['pattern']
-
+    post_data: dict = request.get_json()
     result = {}
-    gists = gists_for_user(username)
+    try:
+        validate_payload: PayloadValidator = PayloadValidator(post_data)
+        validate_payload.validate_api_payload()
 
-    for gist in gists:
-        # TODO: Fetch each gist and check for the pattern
-        pass
+        username = post_data["username"]
+        pattern = post_data["pattern"]
 
-    result['status'] = 'success'
-    result['username'] = username
-    result['pattern'] = pattern
-    result['matches'] = []
+        gists: dict = gists_for_user(username)
+
+        validate_response: ResponseValidator = ResponseValidator(gists)
+        validate_response.validate_response()
+
+        gists_helper = GistsHelper(gists=gists, pattern=pattern)
+        matches = gists_helper.match_patterns()
+
+        result["status"] = "success"
+        result["username"] = username
+        result["pattern"] = pattern
+        result["matches"] = matches
+
+    except API_EXCEPTIONS as exception:
+        result["error"] = exception.args[0]
+        result["status"] = "failed"
 
     return jsonify(result)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=9876)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=9876)
