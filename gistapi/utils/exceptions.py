17753b6480745c064efcc97184a09d9019067278
Danilo Paes <danilo.souza@ntconsult.com.br>
2022-10-10 10:29:08 +0200

    add: exceptions

---
+++ gistapi/utils/exceptions.py
@@ -0,0 +1,14 @@
+class NoPatternInformed(Exception):
+    pass
+
+
+class NoUsernameInformed(Exception):
+    pass
+
+
+class UserNotFound(Exception):
+    pass
+
+
+class InvalidPattern(Exception):
+    pass
